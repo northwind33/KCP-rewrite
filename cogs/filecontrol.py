daa79f3fe8342477bf74dcd9f03468de63416e39
rainy10 <64578776+rainy10@users.noreply.github.com>
2022-01-05 20:34:00 +0900

    Create filecontrol.py

---
+++ cogs/filecontrol.py
@@ -0,0 +1,10 @@
+import discord
+from discord.ext import commands
+
+class FileControl(commands.Cog, name="filecontrol"):
+    def __init__(self, bot):
+        self.bot = bot
+
+
+def setup(bot):
+    bot.add_cog(FileControl(bot))
