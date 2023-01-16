5681c971e5b64a4aa3db9f92a02f7b4d5e870b49
cart324 <63339005+cart324@users.noreply.github.com>
2023-01-16 14:40:38 +0900

    Update craftScanner.py

--- cogs/craftScanner.py
+++ cogs/craftScanner.py
@@ -20,7 +20,7 @@ def to_str(x):
 
 def is_basic_avatar(avatar_url):
     if avatar_url == None:
-        return "https://cdn.discordapp.com/attachments/681058514797461647/1063823139601137724/discord-logo-1100x825.jpg"
+        return "https://cdn.discordapp.com/attachments/681058514797461647/1064418671100956682/image.png"
     else:
         return avatar_url
 
