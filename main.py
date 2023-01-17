e0e32bcd6edc88b27a00f92b983540fe2be12fcf
cart324 <63339005+cart324@users.noreply.github.com>
2023-01-17 19:31:02 +0900

    Update main.py

--- main.py
+++ main.py
@@ -13,12 +13,15 @@ with open('token.txt', 'r') as f:
 
 settings = {}
 
+
 def load_settings():
     global settings
     with open('settings.json', 'r') as f:
         settings = json.load(f)
 
+
 load_settings()
+
 
 def is_whitelisted(user):
     if user in settings['whitelist']:
@@ -26,13 +29,16 @@ def is_whitelisted(user):
     else:
         return False
 
+
 def on_rm_error(func, path, exc_info):
     os.chmod(path, stat.S_IWRITE)
     os.unlink(path)
 
-bot = commands.Bot(command_prefix='!',intents=discord.Intents.all())
 
-paths_list = [["KCP-rewrite/data", "data"], ["KCP-rewrite/cogs", "cogs"]]   # [옮길 파일 위치, 옮겨질 위치]
+bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
+
+paths_list = [["KCP-rewrite/data", "data"], ["KCP-rewrite/cogs", "cogs"]]  # [옮길 파일 위치, 옮겨질 위치]
+
 
 @bot.command()
 async def restart(ctx):
@@ -40,6 +46,8 @@ async def restart(ctx):
         if not is_whitelisted(ctx.author.id):
             return
         await ctx.send('봇이 재시작됩니다.')
+        if os.path.exists("KCP-rewrite"):
+            shutil.rmtree("KCP-rewrite", onerror=on_rm_error)
         os.system("git clone https://github.com/rainy10/KCP-rewrite")
         for paths in paths_list:
             for (path, dirs, files) in os.walk(paths[0]):
@@ -48,10 +56,11 @@ async def restart(ctx):
                     shutil.move(paths[0] + "/" + file_name, paths[1] + "/" + file_name)
         shutil.rmtree("KCP-rewrite", onerror=on_rm_error)
         os.execl(sys.executable, sys.executable, *sys.argv)
-    except:
+    except Exception:
         error_log = traceback.format_exc(limit=None, chain=True)
         cart = bot.get_user(344384179552780289)
-        await cart.send(("-" * 40) + "\n" "사용자 = " + ctx.author.name + "\n" + str(error_log))
+        await cart.send("```" + ("-" * 40) + "\n" "사용자 = " + ctx.author.name + "\n" + str(error_log) + "```")
+
 
 @bot.command()
 async def load(ctx, extension):
@@ -61,12 +70,14 @@ async def load(ctx, extension):
     bot.load_extension(f'cogs.{extension}')
     await ctx.send(f'Loaded {extension}')
 
+
 @bot.command()
 async def unload(ctx, extension):
     if not is_whitelisted(ctx.author.id):
         return
     bot.unload_extension(f'cogs.{extension}')
     await ctx.send(f'Unloaded {extension}')
+
 
 @bot.command()
 async def reload(ctx, extension):
@@ -77,6 +88,8 @@ async def reload(ctx, extension):
     bot.load_extension(f'cogs.{extension}')
     await ctx.send(f'Reloaded {extension}')
 
-[bot.load_extension(f"cogs.{x.replace('.py', '')}") for x in os.listdir("./cogs") if x.endswith('.py') and not x.startswith("_")]
+
+[bot.load_extension(f"cogs.{x.replace('.py', '')}") for x in os.listdir("./cogs") if
+ x.endswith('.py') and not x.startswith("_")]
 
 bot.run(token)
