c536beba982a47e6d1a9dc6e72a248ba8d96b634
cart324 <63339005+cart324@users.noreply.github.com>
2023-12-13 02:15:38 +0900

    Update filecontrol.py

--- cogs/filecontrol.py
+++ cogs/filecontrol.py
@@ -1,10 +1,11 @@
 import discord
 from discord.ext import commands
 
+
 class FileControl(commands.Cog, name="filecontrol"):
     def __init__(self, bot):
         self.bot = bot
 
 
-async def setup(bot):
-    await bot.add_cog(FileControl(bot))
+def setup(bot):
+    bot.add_cog(FileControl(bot))
